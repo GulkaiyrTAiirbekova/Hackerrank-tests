7c963950887911bbba6356f86df7e179ed575a86
Your Name <taiirbekovag@gmail.com>
2025-07-09 22:31:59 +0600

    Java If- Else from hackerrank.com

---
+++ src/Palindrome.java
@@ -0,0 +1,15 @@
+//public class Palindrome {
+//    public boolean isPalondrome(int x){
+//        if (x<0){
+//            return false;
+//        }
+//        //0->49
+//        //1-> 49
+//        //
+//        String s = x;
+//        //c = 'f'
+//        //a -> 97;
+//
+//    }
+//
+//}
